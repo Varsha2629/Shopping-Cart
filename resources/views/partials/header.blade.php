21ce6ec448065dd25195b0e4f4277b38ebffbd09
Varsha Panchal <varsha3051@gmail.com>
2020-11-10 22:25:56 -0500

    Add styles and content
    resources

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Brand </a>
+      </li>
+      <li class="nav-item left">
+        <a class="nav-link" href="#" ><i class="fa fa-shopping-cart"></i> Shopping Cart</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle-o"></i> User Account
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">User Account</a>
+            <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Logout</a>
+        </div>
+      </li>
+      
+    </ul>
+    
+  </div>
+</nav>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -3,6 +3,56 @@
 @section('title')
      Laravel Shopping Cart
 @endsection
+
 @section('content')
-     <h1>It works!!</h1>
+<div class="row">
+     <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+
+            <img src="http://www.kylelambert.com/gallery/stranger-things-3-merch-art/images/stranger_things_3_merch_art_by_kyle_lambert.jpg" alt="..." style="max-height: 150px">
+          
+           <div class="caption">
+             <h3>Product Title</h3>
+               <p class="description">loremloremloremloremlorem loremloremloremloremlorem loremloremloremloremlorem loremloremloremlorem loremloremlorem loremloremlorem</p>
+                <div class="clearfix"> 
+                     <div class="pull-left price">$12</div>
+                    <a href='#' class="btn btn-success pull-right" role="button">Add to Cart</a>
+                </div>
+           </div>
+         </div>
+     </div>
+
+
+     <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+
+            <img src="http://www.kylelambert.com/gallery/stranger-things-3-merch-art/images/stranger_things_3_merch_art_by_kyle_lambert.jpg" alt="..." style="max-height: 150px">
+          
+           <div class="caption">
+             <h3>Product Title</h3>
+               <p class="description">loremloremloremloremlorem loremloremloremloremlorem loremloremloremloremlorem loremloremloremlorem loremloremlorem loremloremlorem</p>
+                <div class="clearfix"> 
+                     <div class="pull-left price">$12</div>
+                    <a href='#' class="btn btn-success pull-right" role="button">Add to Cart</a>
+                </div>
+           </div>
+         </div>
+     </div>
+
+     <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+
+            <img src="http://www.kylelambert.com/gallery/stranger-things-3-merch-art/images/stranger_things_3_merch_art_by_kyle_lambert.jpg" alt="..." style="max-height: 150px">
+          
+           <div class="caption">
+             <h3>Product Title</h3>
+               <p class="description">loremloremloremloremlorem loremloremloremloremlorem loremloremloremloremlorem loremloremloremlorem loremloremlorem loremloremlorem</p>
+                <div class="clearfix"> 
+                     <div class="pull-left price">$12</div>
+                    <a href='#' class="btn btn-success pull-right" role="button">Add to Cart</a>
+                </div>
+           </div>
+         </div>
+     </div>
+</div>
 @endsection
