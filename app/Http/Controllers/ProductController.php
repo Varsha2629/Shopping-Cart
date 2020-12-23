400f7cb37dbe55ae78a61c9f645cd17a0e1badb9
Varsha Panchal <varsha3051@gmail.com>
2020-12-23 14:13:18 -0500

    Finishing Touches

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -38,8 +38,13 @@ class ProductController extends Controller
         $oldCart = Session::has('cart') ? Session ::get('cart') : null;
         $cart = new Cart($oldCart);
         $cart->reduceByOne($id);
-
-        Session::put('cart', $cart);
+        
+        if (count($cart->items) > 0) {
+        Session::put('cart',$cart);
+            }
+            else{
+                Session ::forget('cart');
+            }
         return redirect()->action([ProductController::class, 'getCart']);
     }
 
